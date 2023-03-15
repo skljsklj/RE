36d9e0d1ab360677817638f7f665c91e51467568
skljsklj <93991803+skljsklj@users.noreply.github.com>
2023-03-15 22:45:43 +0100

    Add files via upload

--- lab5.c
+++ lab5.c
@@ -119,3 +119,144 @@ int main(void)
 		}
 	return 0;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <wiringPi.h>
+
+#define LED_PIN 0 // Broj pina na kojem je LED dioda spojena
+
+int main(int argc, char *argv[])
+{
+	int fd = -1, ret;
+	char *tmp1, tmp2[10], ch='t';
+	char devname_head[50] = "/sys/devices/w1_bus_master1/28-00000";
+	char devname_end[10] = "/w1_slave";
+	char dev_name[100];
+	long value;
+	int integer, decimal;
+	char buffer[100];
+	int i,j;
+
+	wiringPiSetup(); // Inicijalizacija wiringPi biblioteke
+	pinMode(LED_PIN, OUTPUT); // Postavljanje pina na koji je LED spojena u modu izlaza
+
+	while(1) // Beskonačna petlja za očitavanje temperature
+	{
+		strcpy(dev_name, devname_head);
+		strcat(dev_name, argv[1]);
+		strcat(dev_name, devname_end);
+
+		if ((fd = open(dev_name, O_RDONLY)) < 0)
+		{
+			perror("Greška pri otvaranju!");
+			exit(1);
+		}
+		ret = read(fd, buffer, sizeof(buffer));
+		if (ret < 0)
+		{
+			perror("Greška pri čitanju!");
+			exit(1);
+		}
+		tmp1 = strchr(buffer, ch);
+		sscanf(tmp1, "t=%s", tmp2);
+		value = atoi(tmp2);
+		integer = value / 1000;
+		decimal = value % 1000;
+
+		printf("Temperatura je %d.%d\n", integer,decimal);
+
+		if (integer >= 25) // Uključivanje LED diode ako je temperatura veća ili jednaka 25 stepeni Celzijusa
+		{
+			for (i = 0; i < 3; i++)
+			{
+				digitalWrite(LED_PIN, HIGH);
+				delay(500);
+				digitalWrite(LED_PIN, LOW);
+				delay(500);
+			}
+		}
+		else // Isključivanje LED diode ako je temperatura manja od 25 stepeni Celzijusa
+		{
+			digitalWrite(LED_PIN, LOW);
+		}
+
+		close(fd);
+	}
+
+	return 0;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <wiringPi.h>
+
+#define LED_PIN 0 // Broj pina na kojem je LED dioda spojena
+
+int main(int argc, char *argv[])
+{
+	int fd = -1, ret;
+	char *tmp1, tmp2[10], ch='t';
+	char devname_head[50] = "/sys/devices/w1_bus_master1/28-00000";
+	char devname_end[10] = "/w1_slave";
+	char dev_name[100];
+	long value;
+	int integer, decimal;
+	char buffer[100];
+	int i,j;
+	int alarm_threshold = 25; // Početna vrednost praga za temperaturni alarm
+
+	wiringPiSetup(); // Inicijalizacija wiringPi biblioteke
+	pinMode(LED_PIN, OUTPUT); // Postavljanje pina na koji je LED spojena u modu izlaza
+
+	while(1) // Beskonačna petlja za očitavanje temperature
+	{
+		strcpy(dev_name, devname_head);
+		strcat(dev_name, argv[1]);
+		strcat(dev_name, devname_end);
+
+		if ((fd = open(dev_name, O_RDONLY)) < 0)
+		{
+			perror("Greška pri otvaranju!");
+			exit(1);
+		}
+		ret = read(fd, buffer, sizeof(buffer));
+		if (ret < 0)
+		{
+			perror("Greška pri čitanju!");
+			exit(1);
+		}
+		tmp1 = strchr(buffer, ch);
+		sscanf(tmp1, "t=%s", tmp2);
+		value = atoi(tmp2);
+		integer = value / 1000;
+		decimal = value % 1000;
+
+		printf("Temperatura je %d.%d\n", integer,decimal);
+
+		if (integer >= alarm_threshold) // Uključivanje LED diode ako je temperatura veća ili jednaka podešenom pragu alarma
+		{
+			for (i = 0; i < 3; i++)
+			{
+				digitalWrite(LED_PIN, HIGH);
+				delay(500);
+				digitalWrite(LED_PIN, LOW);
+				delay(500);
+			}
+		}
+		else // Isključivanje LED diode ako je temperatura manja od podešenog praga alarma
+		{
+			digitalWrite(LED_PIN, LOW);
+		}
+
+		close(fd);
+	}
+
+	return 0;
+}
